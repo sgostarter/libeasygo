bb21c4003b705a081ead70989c0e7edb626b456e
zhaojiuzhou <zhaojiuzhou@guangtuikeji.com>
2021-12-28 10:44:16 +0800

    commerr

--- commerr/errors.go
+++ commerr/errors.go
@@ -3,7 +3,17 @@ package commerr
 import "errors"
 
 var (
-	ErrCanceled        = errors.New("cancelled")
-	ErrUnknown         = errors.New("unknown")
-	ErrInvalidArgument = errors.New("invalidArgument")
+	ErrCanceled          = errors.New("cancelled")
+	ErrUnknown           = errors.New("unknown")
+	ErrInvalidArgument   = errors.New("invalidArgument")
+	ErrInternal          = errors.New("internal")
+	ErrNotFound          = errors.New("notFound")
+	ErrAlreadyExists     = errors.New("alreadyExists")
+	ErrPermissionDenied  = errors.New("permissionDenied")
+	ErrAborted           = errors.New("aborted")
+	ErrOutOfRange        = errors.New("outOfRange")
+	ErrUnimplemented     = errors.New("unimplemented")
+	ErrUnavailable       = errors.New("unavailable")
+	ErrUnauthenticated   = errors.New("unauthenticated")
+	ErrResourceExhausted = errors.New("resourceExhausted")
 )
