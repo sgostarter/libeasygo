aa6c0df985ed86fd6a0a62e0d5956dd2c056b235
zhaojiuzhou <zhaojiuzhou@guangtuikeji.com>
2022-01-06 09:17:33 +0800

    qps

---
+++ qps/qps_test.go
@@ -0,0 +1,48 @@
+package qps
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestQPS(t *testing.T) {
+	qps := New(4)
+
+	var wg sync.WaitGroup
+
+	tEnd := time.Now().Add(time.Second * 8)
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		for idx := 0; time.Until(tEnd) >= 0; idx++ {
+			qps.Inc()
+
+			s := int(time.Until(tEnd).Seconds())
+			time.Sleep(time.Millisecond * time.Duration(s))
+		}
+	}()
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		for time.Until(tEnd) >= 0 {
+			var s string
+			for _, i := range qps.Gets() {
+				s += fmt.Sprintf("%d -", i)
+			}
+
+			s += "\n"
+			t.Log(s)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	wg.Wait()
+}
