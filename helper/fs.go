a3737f5365e20d9450ca909d38c3daf683bd5ab4
patdz <patdz@ymipro.com>
2022-05-10 22:12:18 +0800

    fs

---
+++ helper/fs.go
@@ -0,0 +1,37 @@
+package helper
+
+import "os"
+
+func PathExists(path string) (exists bool, err error) {
+	_, err = os.Stat(path)
+	if err == nil {
+		exists = true
+
+		return
+	}
+
+	if os.IsNotExist(err) {
+		err = nil
+	}
+
+	return
+
+}
+
+func IsDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return s.IsDir()
+}
+
+func IsFile(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return !s.IsDir()
+}
