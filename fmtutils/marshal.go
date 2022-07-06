b2a21336c5801052c79bab161a865dc3d2ae7436
patdz <patdz@ymipro.com>
2022-07-06 15:10:15 +0800

    add marshal util

---
+++ fmtutils/marshal.go
@@ -0,0 +1,27 @@
+package fmtutils
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Marshal ...
+func Marshal(v interface{}) string {
+	dat, err := yaml.Marshal(v)
+	if err == nil {
+		return string(dat)
+	}
+
+	return fmt.Sprintf("%#v", v)
+}
+
+func JsonMarshal(v interface{}) string {
+	dat, err := json.Marshal(v)
+	if err == nil {
+		return string(dat)
+	}
+
+	return fmt.Sprintf("%#v", v)
+}
