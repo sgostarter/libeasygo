5c710fa43199d541e331a1472c15c868abdd2d90
patdz <patdz@ymipro.com>
2022-09-13 18:20:17 +0800

    netutils

---
+++ netutils/dial.go
@@ -0,0 +1,40 @@
+package netutils
+
+import (
+	"context"
+	"crypto/tls"
+	"net"
+	"time"
+)
+
+type TLSConfigModifier func(cfg *tls.Config)
+
+func TLSConfigModifier4InsecureSkipVerify(cfg *tls.Config) {
+	if cfg == nil {
+		return
+	}
+
+	cfg.InsecureSkipVerify = true
+}
+
+func DialTCPWithTimeout(ctx context.Context, useSSL bool, address string, timeout time.Duration,
+	tlsConfigModifier ...TLSConfigModifier) (net.Conn, error) {
+	if !useSSL {
+		return net.DialTimeout("tcp", address, timeout)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	// nolint: gosec
+	tlsConfig := &tls.Config{}
+	for _, modifier := range tlsConfigModifier {
+		modifier(tlsConfig)
+	}
+
+	d := tls.Dialer{
+		Config: tlsConfig,
+	}
+
+	return d.DialContext(ctx, "tcp", address)
+}
