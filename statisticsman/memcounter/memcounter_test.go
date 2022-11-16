0e2642a7c166f8ed4dd9ea782afa9ac98f9606e2
patdz <patdz@ymipro.com>
2022-11-16 20:04:15 +0800

    mem counter ut

--- statisticsman/memcounter/memcounter_test.go
+++ statisticsman/memcounter/memcounter_test.go
@@ -61,3 +61,46 @@ func TestMemCounter2(t *testing.T) {
 		t.Log(sleepN, mc.Count())
 	}
 }
+
+// nolint
+func TestMemCount3(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	mc := NewMemCounter(time.Second, 6)
+
+	counts := make(map[int64]int)
+
+	var compareCount int
+
+	for idx := 0; idx < 1000; idx++ {
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(2000)))
+
+		mc.Inc()
+		counts[time.Now().Unix()]++
+
+		if idx%7 != 0 {
+			continue
+		}
+
+		firstTm := time.Now().Unix() - 5
+
+		count1 := mc.Count()
+
+		var count2 int
+
+		for tm, c := range counts {
+			if tm < firstTm {
+				delete(counts, tm)
+			} else {
+				count2 += c
+			}
+		}
+
+		assert.EqualValues(t, count2, count1)
+
+		compareCount++
+
+		if compareCount%10 == 0 {
+			t.Log("Compare: ", compareCount)
+		}
+	}
+}
