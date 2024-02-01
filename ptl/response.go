8bd9b42dba633df1324c5ca539edc47a03dbb73b
patdz <patdz@ymipro.com>
2024-02-01 08:11:48 +0800

    update ResponseWrapper: RawMessage

--- ptl/response.go
+++ ptl/response.go
@@ -1,14 +1,24 @@
 package ptl
 
 type ResponseWrapper struct {
-	Code    Code        `json:"code"`
-	Message string      `json:"message"`
-	Resp    interface{} `json:"resp,omitempty"`
+	Code       Code        `json:"code"`
+	Message    string      `json:"message"`
+	Resp       interface{} `json:"resp,omitempty"`
+	RawMessage string      `json:"-" yaml:"-"`
 }
 
 func (wr *ResponseWrapper) Apply(code Code, msg string) bool {
 	wr.Code = code
+	wr.RawMessage = msg
 	wr.Message = CodeToMessage(code, msg)
+
+	return wr.Code == CodeSuccess
+}
+
+func (wr *ResponseWrapper) Clone(wro ResponseWrapper) bool {
+	wr.Code = wro.Code
+	wr.RawMessage = wro.RawMessage
+	wr.Message = wro.Message
 
 	return wr.Code == CodeSuccess
 }
